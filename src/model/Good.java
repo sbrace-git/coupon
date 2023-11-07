a0857b4a825a35c8097026cfe1648e163a15aae0
sbrace <sbrace0822@gmail.com>
2023-11-07 23:55:33 +0800

    delete alternativeCouponList

--- src/model/Good.java
+++ src/model/Good.java
@@ -6,7 +6,6 @@ import java.util.List;
 public class Good {
     int price;
     private List<Coupon> couponList;
-    private List<Coupon> alternativeCouponList;
 
     public int getPrice() {
         return price;
@@ -24,18 +23,10 @@ public class Good {
         this.couponList = couponList;
     }
 
-    public List<Coupon> getAlternativeCouponList() {
-        return alternativeCouponList;
-    }
-
-    public void setAlternativeCouponList(List<Coupon> alternativeCouponList) {
-        this.alternativeCouponList = alternativeCouponList;
-    }
 
     public Good(int price) {
         this.price = price;
         this.couponList = new ArrayList<>();
-        this.alternativeCouponList = new ArrayList<>();
     }
 
     @Override
