423dad988bc85261838b79f876e94fef7eb1e587
sbrace <sbrace0822@gmail.com>
2023-11-07 22:30:32 +0800

    fixed test6

--- src/Main.java
+++ src/Main.java
@@ -12,7 +12,27 @@ public class Main {
                 .mapToObj(Good::new).collect(Collectors.toList());
         List<Coupon> unUsedCouponsList = Arrays.stream(coupons).boxed().sorted(Comparator.reverseOrder())
                 .map(Coupon::new).collect(Collectors.toList());
+        IntSummaryStatistics result = goodSetCouponList(goodList, unUsedCouponsList);
 
+        List<Good> goodList2 = Arrays.stream(goodPrices).boxed().sorted(Comparator.reverseOrder())
+                .map(Good::new).collect(Collectors.toList());
+        List<Coupon> unUsedCouponsList2 = Arrays.stream(coupons).boxed().sorted(Comparator.reverseOrder())
+                .map(Coupon::new).collect(Collectors.toList());
+        IntSummaryStatistics result2 = goodSetCouponList(goodList2, unUsedCouponsList2);
+
+        if (result2.getSum() >= result.getSum() && result2.getCount() >= result.getCount()) {
+            goodList = goodList2;
+            unUsedCouponsList = unUsedCouponsList2;
+            result = result2;
+        }
+        System.out.println("优惠组合 : " + goodList.stream().map(Good::toString).collect(Collectors.joining(", ")));
+        System.out.println("优惠金额 : " + result.getSum());
+        System.out.println("使用优惠卷数量 : " + result.getCount());
+        System.out.printf("未使用的优惠卷 : %s%n%n", unUsedCouponsList);
+        return result;
+    }
+
+    private IntSummaryStatistics goodSetCouponList(List<Good> goodList, List<Coupon> unUsedCouponsList) {
         for (Good good : goodList) {
             int couponAmountMax = 0;
             for (int outIndex = 0; outIndex < unUsedCouponsList.size(); outIndex++) {
@@ -36,13 +56,8 @@ public class Main {
             }
             good.getCouponList().forEach(unUsedCouponsList::remove);
         }
-
-        System.out.println("优惠组合 : " + goodList.stream().map(Good::toString).collect(Collectors.joining(", ")));
         IntSummaryStatistics intSummaryStatistics = goodList.stream().map(Good::getCouponList).flatMap(List::stream)
                 .mapToInt(Coupon::getAmount).summaryStatistics();
-        System.out.println("优惠金额 : " + intSummaryStatistics.getSum());
-        System.out.println("使用优惠卷数量 : " + intSummaryStatistics.getCount());
-        System.out.printf("未使用的优惠卷 : %s%n%n", unUsedCouponsList);
         return intSummaryStatistics;
     }
 }
